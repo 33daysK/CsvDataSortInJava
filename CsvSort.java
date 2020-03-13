e53d1da52f961bc1c671e31b9e68174a3b53a4f5
33daysK <enjingorira@gmail.com>
2020-03-13 16:29:16 +0900

    refactoring　CsvSort
    コメントの内容を追加、修正しました

--- CsvSort.java
+++ CsvSort.java
@@ -1,6 +1,11 @@
-/*  ユーザが選んだ項目と種別でソートするプログラム。
- *  メイン部分はなるべく簡潔にしました
+/**  
+ *  ここがメインクラスです。
  *  
+ *  @auther:33daysK
+ *  @version バージョン1.0 2020/03/13
+ *  ユーザがソート方法を選んで、読み込んだデータをソートします。
+ *  ソートしたデータはCSV形式ファイルと画面に出力します。
+ *  メイン部分はなるべく簡潔にしたかったのでほとんどの操作はほかのクラスが担当しています。
  */
 import java.util.List;
 
@@ -12,21 +17,19 @@ public class CsvSort{
 			Option.output(choice);
 		}
 		
-		//  項目を追加したらInputとMakeを変更すること
 		System.out.println("並べ替えたい項目は何ですか？");
 		System.out.print("[0:番号 1:クラス 2:年齢 3:点数] > ");
 		int selectItem = Input.checkInt(3);
-
-		//  項目を追加したらInputとMakeを変更すること
+		  
 		System.out.println("並べ替え種別を指定してください");
 		System.out.print("[0:昇順 1:降順] > ");
-		boolean trueAscending = Input.checkBoolean();
+		boolean isAscending = Input.checkBoolean();
 		
-		// 読み取った情報を元にStudent型のリストを作成する。
-		// ソートはwrite()の中で行っている
+		// この出力文字列の間のスペースはデータをそろえるためのものです。
+		// 少々強引ですがいい方法を思いつきませんでした。
 		List<Student> studentList = WorkFile.read();
 	    System.out.println(" 番号     名前             クラス   年齢   点数");
-		WorkFile.write(selectItem,trueAscending,studentList);
+		WorkFile.write(selectItem,isAscending,studentList);
 		
 	}
 }
